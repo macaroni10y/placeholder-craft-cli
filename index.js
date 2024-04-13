a69702a5db5529ab6f428ec6f781099d5289b4dd
macaroni10y <56068155+macaroni10y@users.noreply.github.com>
2024-04-13 12:16:01 +0900

    refactor

--- index.js
+++ index.js
@@ -1,56 +1,57 @@
 #!/usr/bin/env node
 
-const { createCanvas } = require('canvas');
+const {createCanvas} = require('canvas');
 const fs = require('fs');
 const yargs = require('yargs');
 
 const argv = yargs
     .option('width', {
-      alias: 'w',
-      describe: 'The width of the image',
-      type: 'number',
-      demandOption: true,
+        alias: 'w',
+        describe: 'The width of the image',
+        type: 'number',
+        demandOption: true,
     })
     .option('height', {
-      alias: 'h',
-      describe: 'The height of the image',
-      type: 'number',
-      demandOption: true,
+        alias: 'h',
+        describe: 'The height of the image',
+        type: 'number',
+        demandOption: true,
     })
     .option('bgColor', {
-      alias: 'b',
-      describe: 'Background color',
-      type: 'string',
-      default: 'grey',
+        alias: 'b',
+        describe: 'Background color',
+        type: 'string',
+        default: 'grey',
     })
     .option('textColor', {
-      alias: 't',
-      describe: 'Text color',
-      type: 'string',
-      default: 'white',
+        alias: 't',
+        describe: 'Text color',
+        type: 'string',
+        default: 'white',
     })
     .option('filename', {
-      alias: 'f',
-      describe: 'The output filename',
-      type: 'string',
-      default: 'placeholder.png',
+        alias: 'f',
+        describe: 'The output filename',
+        type: 'string',
+        default: 'placeholder.png',
     })
+    .help()
     .argv;
 
-function createPlaceholderImage(width, height, bgColor, textColor, filename) {
-  const canvas = createCanvas(width, height);
-  const ctx = canvas.getContext('2d');
-  ctx.fillStyle = bgColor;
-  ctx.fillRect(0, 0, width, height);
-  ctx.fillStyle = textColor;
-  const fontSize = Math.min(width, height) / 2;
-  ctx.font = `${fontSize}px Arial`;
-  ctx.textAlign = 'center';
-  ctx.textBaseline = 'middle';
-  ctx.fillText(`${width} * ${height}`, width / 2, height / 2, width - 20);
+const createPlaceholderImage = ({width, height, bgColor, textColor, filename}) => {
+    const canvas = createCanvas(width, height);
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = bgColor;
+    ctx.fillRect(0, 0, width, height);
+    ctx.fillStyle = textColor;
+    const fontSize = Math.min(width, height) / 2;
+    ctx.font = `${fontSize}px Arial`;
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.fillText(`${width} * ${height}`, width / 2, height / 2, width - 20);
 
-  const buffer = canvas.toBuffer('image/png');
-  fs.writeFileSync(filename, buffer);
-}
+    const buffer = canvas.toBuffer('image/png');
+    fs.writeFileSync(filename, buffer);
+};
 
-createPlaceholderImage(argv.width, argv.height, argv.bgColor, argv.textColor, argv.filename);
+createPlaceholderImage(argv);
